90ab736b0762014114d9fc19c524c459383df690
dylan <dylanvicchio@gmail.com>
2025-11-11 18:41:26 -0300

    Agrege los metodos de la reserva y sus atributos faltantes

--- ProyectoFinalHotel/src/Pasajero.java
+++ ProyectoFinalHotel/src/Pasajero.java
@@ -1,12 +1,18 @@
+import java.util.HashMap;
+
 public class Pasajero extends Persona {
 
     private String origen;
     private String domicilioOrigen;
+    private static int estadias = 0;
+    private HashMap<Integer, Ocupacion> historialEstadia = new HashMap<>();
 
-    public Pasajero(String nombre, String apellido, int numeroCell, int dni, int direccion, String mail, String origen, String domicilioOrigen) {
-        super(nombre, apellido, numeroCell, dni, direccion, mail);
+    public Pasajero(int id, String nombre, String apellido, int numeroCell, int dni, int direccion, String mail, String origen, String domicilioOrigen, Ocupacion ocupacion) {
+        super(id, nombre, apellido, numeroCell, dni, direccion, mail);
         this.origen = origen;
         this.domicilioOrigen = domicilioOrigen;
+        estadias++;
+        this.historialEstadia.put(estadias, ocupacion);
     }
 
     public String getOrigen() {
@@ -25,11 +31,26 @@ public class Pasajero extends Persona {
         this.domicilioOrigen = domicilioOrigen;
     }
 
+    public void addHistorial (Ocupacion ocupacion){
+        estadias++;
+        this.historialEstadia.put(estadias, ocupacion);
+    }
+
+    public String getHistorial() {
+        StringBuilder str = new StringBuilder();
+
+        for (int i = 0; i < this.historialEstadia.size(); i++) {
+            str.append(this.historialEstadia.get(i).toString());
+        }
+        return str.toString();
+    }
+
     @Override
     public String toString() {
-        return "Pasajero{" + '\'' + super.toString() +
+        return "Pasajero{" + super.toString() + '\'' +
                 "origen='" + origen + '\'' +
                 ", domicilioOrigen='" + domicilioOrigen + '\'' +
+                ", historialEstadia=" + getHistorial() +
                 '}';
     }
 }
